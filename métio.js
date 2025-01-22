b296e44fcafa74a5eef8807a7ff9fb8b0a14b87d
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-01-22 23:27:22 +0100

    modification

--- métio.js
+++ métio.js
@@ -13,17 +13,44 @@
   }
   fetchCityData('BENI MELLAL');*/
   const apiKey="167aba0344bc2572284206b1dcc73ce3";
-  const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
-  async function chckWeather(){
-    const response=await fetch(apiUrl +`&appid=${apiKey}`);
+  const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+  const searchBox=document.querySelector(".search input");
+  const searchbtn=document.querySelector(".search button");
+  const weatherIcon=document.querySelector(".weather-icon");
+
+  async function chckWeather(city){
+    const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data =await response.json();
     console.log(data);
     document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temperature").innerHTML=data.name +"°C";
+    document.querySelector(".temperature").innerHTML= data.main.temp  +"°C";
     document.querySelector(".Humidité").innerHTML=data.main.humidity +"%";
     document.querySelector(".wind").innerHTML=data.wind.speed +"KM/h";
+    
+    if(data.weather[0].main == "clouds"){
+      weatherIcon.src="image/cloudy-day-2.svg"
+    } 
+    else if(data.weather[0].main == "Clear"){
+      weatherIcon.src="image/pluis.svg"
 
-  }
-  chckWeather();
+    }
+    else if(data.weather[0].main == "Rain"){
+      weatherIcon.src="image/nuage.svg"
 
+    }
+    else if(data.weather[0].main == "Drizzle"){
+      weatherIcon.src="image/soleil.svg"
+
+    }
+    else if(data.weather[0].main == "Mist"){
+      weatherIcon.src="image/cloudy-night-2.svg"
+
+    }}
+  searchbtn.addEventListener("click" ,()=>{
+    chckWeather(searchBox.value);
+
+  })
+  
+  
   
