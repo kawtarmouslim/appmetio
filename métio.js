4d08848fca0c680b3eb3b9ac4d4c6424bbd5c333
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-01-24 16:19:19 +0100

    jours

--- métio.js
+++ métio.js
@@ -1,116 +1,100 @@
+const apiKey = "167aba0344bc2572284206b1dcc73ce3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrls = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
 
-  /*async function fetchCityData(cityName) {
-    const url = `https://api.api-ninjas.com/v1/city?name=${cityName}`;
-    const headers = new Headers();
-    headers.set('X-Api-Key', 'AwOxztTkm1VcnPYr3YNwag==tk5oBTh4eKxnZE5b');
-    try {
-      const response = await fetch(url, { headers });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Erreur:', error);
-    }
-  }
-  fetchCityData('BENI MELLAL');*/
+const searchBox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
+async function detailJours(name) {
+  const responsee = await fetch(apiUrls + name + `&appid=${apiKey}`);
+  const dataDetailJours = await responsee.json();
+  console.log(dataDetailJours);
 
-  const apiKey="167aba0344bc2572284206b1dcc73ce3";
-  
-  const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-  const apiUrls="https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
+  for (let i = 1; i <= 4; i++) {
+    const dayData = dataDetailJours.list[i * 8]; 
+    const dayElement = document.querySelector(`.day:nth-child(${i})`);
 
-  const searchBox=document.querySelector(".search input");
-  const searchbtn=document.querySelector(".search button");
-  const weatherIcon=document.querySelector(".weather-icon");
-
-  async function detailJours(name) {
-    const responsee=await fetch(apiUrls + name + `&appid=${apiKey}`);
-    var dataDetailJours =await responsee.json();
-    console.log(dataDetailJours);
-    
-    document.querySelector(".tempv").innerHTML= dataDetailJours.list[1].main.temp  +"°C";
-    document.querySelector(".Humiditev").innerHTML=dataDetailJours.list[1].main.humidity +"%";
-    document.querySelector(".windv").innerHTML=dataDetailJours.list[1].wind.speed +"KM/h";
-
-    document.querySelector(".temperaturel").innerHTML= dataDetailJours.list[25].main.temp  +"°C";
-    document.querySelector(".Humiditel").innerHTML=dataDetailJours.list[25].main.humidity +"%";
-    document.querySelector(".windl").innerHTML=dataDetailJours.list[25].wind.speed +"KM/h";
-   
-    document.querySelector(".temperatureema").innerHTML= dataDetailJours.list[28].main.temp  +"°C";
-    document.querySelector(".Humiditema").innerHTML=dataDetailJours.list[28].main.humidity +"%";
-    document.querySelector(".windma").innerHTML=dataDetailJours.list[28].wind.speed +"KM/h";
-
+    dayElement.querySelector(".temperature").innerHTML = dayData.main.temp + "°C";
+    dayElement.querySelector(".Humidite").innerHTML = dayData.main.humidity + "%";
+    dayElement.querySelector(".wind").innerHTML = dayData.wind.speed + " KM/h";
 
     
-    const weatherType = dataDetailJours.list[1].weather[0].main;
+    const weatherType = dayData.weather[0].main;
+    const iconElement = dayElement.querySelector("img");
 
     if (weatherType === "Clouds") {
-      weatherIcon.src = "image/nuage.svg";
+      iconElement.src = "image/nuage.svg";
     } else if (weatherType === "Clear") {
-      weatherIcon.src = "image/clair.svg";
+      iconElement.src = "image/clair.svg";
     } else if (weatherType === "Rain") {
-      weatherIcon.src = "image/pluis.svg";
+      iconElement.src = "image/pluis.svg";
     } else if (weatherType === "Sunny") {
-      weatherIcon.src = "image/soleil.svg";
+      iconElement.src = "image/soleil.svg";
     } else if (weatherType === "Mist") {
-      weatherIcon.src = "image/cloudy-night-2.svg";
+      iconElement.src = "image/cloudy-night-2.svg";
     }
-    
-    
   }
-  
+}
 
-  async function chckWeather(city){
-    const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data =await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temperature").innerHTML= data.main.temp  +"°C";
-    document.querySelector(".Humidité").innerHTML=data.main.humidity +"%";
-    document.querySelector(".wind").innerHTML=data.wind.speed +"KM/h";
+async function chckWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const data = await response.json();
+  console.log(data);
 
-    document.querySelector(".temperaturee").innerHTML= data.main.temp  +"°C";
-    document.querySelector(".Humidite").innerHTML=data.main.humidity +"%";
-    document.querySelector(".wind1").innerHTML=data.wind.speed +"KM/h";
-    
-    
-    if(data.weather[0].main == "nuages"){
-      weatherIcon.src="image/nuage.svg"
-    } 
-    else if(data.weather[0].main == "Clear"){
-      weatherIcon.src="image/clair.svg"
+ 
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
+  document.querySelector(".Humidité").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "KM/h";
 
-    }
-    else if(data.weather[0].main == "Plui"){
-      weatherIcon.src="image/pluis.svg"
-
-    }
-    else if(data.weather[0].main == "soleil"){
-      weatherIcon.src="image/soleil.svg"
-
-    }
-    else if(data.weather[0].main == "Mist"){
-      weatherIcon.src="image/cloudy-night-2.svg"
-
-    }}
-  searchbtn.addEventListener("click" ,()=>{
-    chckWeather(searchBox.value);
-    detailJours(searchBox.value);
-    temp();
-
-  })
-  const weekday = ["d","Lundi","Mardi","Mercredi","Jeudi","Vendredi"];
-  function temp() {
-    const date=new Date();
-    let jour=weekday[date.getDay()];
-    let lesJours = document.querySelectorAll('.jour');
-    let found = false;
-    lesJours.forEach((element) =>{
-      if(element.innerHTML == jour){
-        found = true;
-        element.innerHTML = "aujourdui";
-      }
-    })
+ 
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "image/nuage.svg";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "image/clair.svg";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "image/pluis.svg";
+  } else if (data.weather[0].main === "Sunny") {
+    weatherIcon.src = "image/soleil.svg";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "image/cloudy-night-2.svg";
   }
+}
+//fonction afficher les jours
+function generateForecastDays() {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const forecastContainer = document.querySelector(".forecast");
+
+ 
+  forecastContainer.innerHTML = "";
 
   
+  const today = new Date();
+  const dayIndex = today.getDay(); 
+
+  for (let i = 1; i <= 4; i++) {
+    const nextDayIndex = (dayIndex + i) % 7; 
+    const nextDay = days[nextDayIndex];
+
+    const dayElement = document.createElement("div");
+    dayElement.classList.add("day");
+
+    dayElement.innerHTML = `
+      <h4 class="jour">${nextDay}</h4>
+      <img src="image/nuage.svg" alt="${nextDay}" class="weather-icon">
+      <p class="temperature"></p>
+      <p class="Humidite"></p>
+      <p class="wind"></p>
+    `;
+
+    forecastContainer.appendChild(dayElement);
+  }
+}
+
+searchbtn.addEventListener("click", () => {
+  const city = searchBox.value;
+  chckWeather(city);
+  detailJours(city);
+  generateForecastDays();
+});
